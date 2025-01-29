4b940f177a42ab6d208d8534c1d71eb65435d2c4
kartikkumar221a <kartikkumar221a@gmail.com>
2025-01-29 17:52:03 +0000

    creating middlewares

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -2,3 +2,28 @@ import { resend } from "@/lib/resend";
 import VerificationEmail from "@/../emails/verificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
 
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Mystry Message | Verification Code",
+            react: VerificationEmail({ username, otp: verifyCode })
+        })
+
+        return {
+            success: true,
+            message: "Email sent successfully"
+        }
+    } catch (error: any) {
+        console.error("Error sending verification email", error);
+        return {
+            success: false,
+            message: "Failed to send verification email"
+        }
+    }
+}
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/signin',
+    '/signup',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+}
